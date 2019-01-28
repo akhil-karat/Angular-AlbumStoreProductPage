541a443aef5e56af22d072c821e6b9c2fe8f18d6
akhil <akhil3k@gmail.com>
2019-01-27 21:13:53 -0800

    fixed service errorsa

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,12 +8,12 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  public _albumInfo;
+  public albumInfo;
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(response => {
-      this._albumInfo = response;
+      this.albumInfo = response;
     });
   }
 
